13e87f2533ea02c18e2913e2ef1ce8ff96ed2732
Navin3d <smnavin65@gmail.com>
2023-07-23 16:34:56 +0530

    removed asset transfer contract

--- chaincode/index.js
+++ chaincode/index.js
@@ -6,11 +6,7 @@
 
 'use strict';
 
-const assetTransfer = require('./lib/assetTransfer');
 const ComprehensiveSmartContract = require("./lib/ComprehesiveSmartContract");
-
-// module.exports.AssetTransfer = assetTransfer;
-// module.exports.contracts = [assetTransfer];
 
 module.exports.ComprehensiveSmartContract = ComprehensiveSmartContract;
 module.exports.contracts = [ComprehensiveSmartContract];
